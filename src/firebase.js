b8f2a8e9a8b972ae65d3a82068e1dc7caa658695
seungiminRyu <fbtmdals011@naver.com>
2023-01-18 18:27:35 +0900

    Edit: firebaseConfig 값 환경변수로 대체

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtMR3k0E3Cerl1h56BsxCGf3B1YnxS5sk",
-    authDomain: "twitter-clone-2dd80.firebaseapp.com",
-    projectId: "twitter-clone-2dd80",
-    storageBucket: "twitter-clone-2dd80.appspot.com",
-    messagingSenderId: "243508196566",
-    appId: "1:243508196566:web:62ea863062b9a7f2f08577",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
