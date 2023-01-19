ac5f54bd149919f98360e9b8342a05cecf74a6ea
seungiminRyu <fbtmdals011@naver.com>
2023-01-19 10:18:00 +0900

    Create: 프로젝트 구조 세팅

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 import firebase from "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
