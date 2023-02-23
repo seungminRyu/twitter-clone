17d19c647da575abbdf84e48dd7ed0996070392f
seungiminRyu <fbtmdals011@naver.com>
2023-02-24 01:50:17 +0900

    Add: Auth 컴포넌트 기본 요소들 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Auth() {
-    return <div>Auth</div>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="example@example.com"
+                    value={email}
+                    onChange={onChange}
+                    required
+                ></input>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="비밀번호"
+                    value={password}
+                    onChange={onChange}
+                    required
+                ></input>
+                <input type="submit" value="로그인"></input>
+            </form>
+            <button>구글 계정으로 로그인</button>
+            <button>깃허브 계정으로 로그인</button>
+        </div>
+    );
 }
 
 export default Auth;
