60bb0f83833d51a48e372980dfb940a079569c8e
seungiminRyu <fbtmdals011@naver.com>
2023-03-01 23:26:12 +0900

    Add: Profile 페이지에 로그아웃 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,20 @@
+import { authService } from "firebaseClient";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-    return <></>;
+    const navigate = useNavigate();
+
+    const onLogoutBtnClick = async () => {
+        await authService.signOut();
+        navigate("/");
+    };
+
+    return (
+        <>
+            <button onClick={onLogoutBtnClick}>로그아웃</button>
+        </>
+    );
 }
 
 export default Profile;
