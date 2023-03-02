1a4474c9c0cb9201babf40be7cbad021961a2408
seungiminRyu <fbtmdals011@naver.com>
2023-03-03 01:44:35 +0900

    Add: Home에 트위터 form 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
-    return <div>Home</div>;
+    const [twitter, setTwitter] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const onChange = (e) => {
+        const value = e.target.value;
+        setTwitter(value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    type="text"
+                    placeholder="현재 무슨 생각을 하고 계신가요?"
+                    value={twitter}
+                    maxLength={120}
+                ></input>
+                <input
+                    type="submit"
+                    value="
+                    트위터 생성"
+                ></input>
+            </form>
+        </div>
+    );
 }
 
 export default Home;
