a9ffce93c18309ed8469f61ff3c58245db953609
seungiminRyu <fbtmdals011@naver.com>
2023-03-06 01:06:38 +0900

    Add: 서버에 트위터 생성 기능 추가

--- src/firebaseClient.js
+++ src/firebaseClient.js
@@ -2,18 +2,19 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtMR3k0E3Cerl1h56BsxCGf3B1YnxS5sk",
-    authDomain: "twitter-clone-2dd80.firebaseapp.com",
-    projectId: "twitter-clone-2dd80",
-    storageBucket: "twitter-clone-2dd80.appspot.com",
-    messagingSenderId: "243508196566",
-    appId: "1:243508196566:web:62ea863062b9a7f2f08577",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 export const firebaseInstance = firebase;
-
 export const authService = firebase.auth();
+export const db = getFirestore(app);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,28 @@
+import { db } from "firebaseClient";
 import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore/lite";
 
 function Home() {
-    const [twitter, setTwitter] = useState("");
+    const [tweet, setTweet] = useState("");
+    const ref = collection(db, "tweet");
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        try {
+            await addDoc(ref, {
+                tweet,
+                createdAt: Date.now(),
+            });
+            alert("트위터 생성 완료");
+            setTweet("");
+        } catch (e) {
+            alert("트위터 생성중 에러 발생: ", e.message);
+        }
     };
 
     const onChange = (e) => {
         const value = e.target.value;
-        setTwitter(value);
+        setTweet(value);
     };
 
     return (
@@ -19,7 +32,7 @@ function Home() {
                     onChange={onChange}
                     type="text"
                     placeholder="현재 무슨 생각을 하고 계신가요?"
-                    value={twitter}
+                    value={tweet}
                     maxLength={120}
                 ></input>
                 <input
