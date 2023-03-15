bc17a8d86e873b0208064e6448b76eca1daa8627
seungiminRyu <fbtmdals011@naver.com>
2023-03-16 02:05:45 +0900

    Add: TweetItem 수정, 삭제 기능 추가

--- src/components/TweetItem.js
+++ src/components/TweetItem.js
@@ -1,14 +1,58 @@
-import React from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "firebaseClient";
+import React, { useState } from "react";
 
 function TweetItem(props) {
     const { tweet, isOwner } = props;
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweet.text);
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm("트위터를 삭제하시겠습니까?");
+        if (ok) {
+            await deleteDoc(doc(db, "tweets", tweet.id));
+        }
+    };
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onChange = (e) => {
+        const value = e.target.value;
+        setNewTweet(value);
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await updateDoc(doc(db, "tweets", tweet.id), {
+            text: newTweet,
+        });
+        setEditing(false);
+    };
+
     return (
         <div>
-            <h4>{tweet.text}</h4>
-            {isOwner && (
+            {editing ? (
                 <>
-                    <button>삭제</button>
-                    <button>수정</button>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            placeholder="트위트 수정하기"
+                            onChange={onChange}
+                            value={newTweet}
+                        />
+                        <input type="submit" value="수정하기" />
+                    </form>
+                    <button onClick={toggleEditing}>취소</button>
+                </>
+            ) : (
+                <>
+                    <h4>{tweet.text}</h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>삭제</button>
+                            <button onClick={toggleEditing}>수정</button>
+                        </>
+                    )}
                 </>
             )}
         </div>
